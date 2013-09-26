52a073645eb3672cd6162f938532763a353522bb
koren@pukogames.com <koren@pukogames.com>
2013-09-26 13:47:58 +0200

    Getting some fun with a sphere and rotating it smoothly independant from the framerate

--- src/main.cpp
+++ src/main.cpp
@@ -12,12 +12,30 @@ int main(int argc, char** argv){
     }
 
     video::IVideoDriver* driver = device->getVideoDriver();
-    scene::ISceneManager *gestionnaire = device->getSceneManager();
+    scene::ISceneManager *smgr = device->getSceneManager();
 
-    gestionnaire->addCameraSceneNode(0, core::vector3df(0,0,0), core::vector3df(5,0,0));
+    smgr->addCameraSceneNode(0, core::vector3df(0,0,-100), core::vector3df(0,0,0));
+    scene::ISceneNode * node = smgr->addSphereSceneNode();
+    if (node){
+        node->setPosition(core::vector3df(0,0,0));
+        node->setScale(core::vector3df(2.f, 2.f, 2.f));
+        node->setMaterialTexture(0, driver->getTexture("../textures/earth.jpg"));
+        node->setMaterialFlag(video::EMF_LIGHTING, false);
+    }
+
+    float rotationY = 0.f;
+    float rotationSpeed = 0.01f;
+    u32 m_Time = device->getTimer()->getTime();
     while(device->run()){
+        u32 m_DeltaTime = device->getTimer()->getTime() - m_Time;
+        m_Time += m_DeltaTime;
+
+        rotationY += rotationSpeed * m_DeltaTime;
+        if(rotationY > 360.f) rotationY = rotationY - 360.f;
+        node->setRotation(core::vector3df(0.f, rotationY, 0.f));
+
         driver->beginScene(true, true, video::SColor(255,100,100,100));
-        gestionnaire->drawAll();
+        smgr->drawAll();
         driver->endScene();
     }
     device->drop();
