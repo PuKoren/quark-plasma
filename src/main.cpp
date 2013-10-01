61ec1706204fa9680633f5876039850d018c7fb6
PuKoren <koren@pukogames.com>
2013-10-01 18:03:10 +0200

    Atmosphere

--- src/main.cpp
+++ src/main.cpp
@@ -35,31 +35,57 @@ int main(int argc, char** argv){
         sun->addChild(earth);
         earth->setPosition(core::vector3df(1000.f,0,0));
         earth->setMaterialTexture(0, driver->getTexture("../textures/earth.jpg"));
-        scene::ISceneNodeAnimator * animator = smgr->createFlyCircleAnimator(core::vector3df(0,0,0), 1000.f, 0.00003f, core::vector3df(0.f, -1.f, 0.f));
-        if (animator){
-            earth->addAnimator(animator);
-            animator->drop();
+
+        video::ITexture* normalMap = driver->getTexture("../textures/earth_bump.jpg");
+        if (normalMap){
+            driver->makeNormalMapTexture(normalMap, 100.0f);
+            earth->setMaterialTexture (1, normalMap);
+            earth->setMaterialType(video::EMT_NORMAL_MAP_TRANSPARENT_VERTEX_ALPHA);
         }
+
+        scene::ISceneNodeAnimator* anim = smgr->createRotationAnimator(core::vector3df(0,0.01f,0));
+        earth->addAnimator(anim);
+        anim->drop();
+
+        scene::ISceneNode * earth_clouds = smgr->addSphereSceneNode(25.75f, 64);
+        earth->addChild(earth_clouds);
+        earth_clouds->setPosition(core::vector3df(0.f,0.f,0.f));
+        earth_clouds->setMaterialTexture(0, driver->getTexture("../textures/earth_clouds.jpg"));
+        earth_clouds->setMaterialType(video::EMT_TRANSPARENT_ADD_COLOR);
+
+        earth_clouds->getMaterial(0).SpecularColor.set(255,0,40,110);
+        earth_clouds->getMaterial(0).Shininess = 2.f;
+        
+        
+    }
+
+    //mars
+    //earth
+    scene::ISceneNode * mars = smgr->addSphereSceneNode(40.f, 64);
+    if (mars){
+        sun->addChild(mars);
+        mars->setPosition(core::vector3df(2000.f,0,0));
+        mars->setMaterialTexture(0, driver->getTexture("../textures/mars.jpg"));
     }
 
     //moon
-    scene::ISceneNode * moon = smgr->addSphereSceneNode(1.f, 32);
+    scene::ISceneNode * moon = smgr->addSphereSceneNode(2.f, 32);
     if (moon){
         earth->addChild(moon);
         moon->setPosition(core::vector3df(0,0,0));
         moon->setMaterialTexture(0, driver->getTexture("../textures/moon.jpg"));
         scene::ISceneNodeAnimator * animator = smgr->createFlyCircleAnimator(core::vector3df(0,0,0), 50.0f, 0.0003f, core::vector3df(0.3f, -1.f, 0.f));
-        if (animator){
-            moon->addAnimator(animator);
-            animator->drop();
-        }
+        scene::ISceneNodeAnimator* anim = smgr->createRotationAnimator(core::vector3df(0,0.8f,0));
+        moon->addAnimator(animator);
+        moon->addAnimator(anim);
+        animator->drop();
+        anim->drop();
     }
 
     //camera
-    scene::ICameraSceneNode* camera = smgr->addCameraSceneNodeFPS(0, 100.0f, 0.06f);
-    camera->setPosition(core::vector3df(0,1100,0));
-    
-    
+    scene::ICameraSceneNode* camera = smgr->addCameraSceneNodeFPS(0, 100.0f, 0.1f);
+    camera->setPosition(core::vector3df(earth->getPosition().X,0,-100));
+    camera->setTarget(earth->getPosition());
 
     //skybox
     scene::ISceneNode* skybox = smgr->addSkyBoxSceneNode(
@@ -70,18 +96,7 @@ int main(int argc, char** argv){
         driver->getTexture("../textures/ESO_-_Milky_Way_Front.bmp"),
         driver->getTexture("../textures/ESO_-_Milky_Way_Back.bmp"));
 
-    float rotationY = 0.f;
-    float rotationSpeed = 0.05f;
-    u32 m_Time = device->getTimer()->getTime();
     while(device->run()){
-        u32 m_DeltaTime = device->getTimer()->getTime() - m_Time;
-        m_Time += m_DeltaTime;
-
-        rotationY += rotationSpeed * m_DeltaTime;
-        sun->setRotation(core::vector3df(0.f, -rotationY/10, 0.f));
-        earth->setRotation(core::vector3df(0.f, rotationY, 0.f));
-        moon->setRotation(core::vector3df(10.f, rotationY*4, 0.f));
-
         driver->beginScene(true, true, video::SColor(255,100,100,100));
         smgr->drawAll();
         driver->endScene();
