f06e4ca88274d2ff83c4bd303f6fe87b3e451f66
PuKoren <koren@pukogames.com>
2013-10-04 15:53:39 +0200

    Rely less on visual studio configuration, more on code and preprocessors...

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,10 @@ using namespace irr;
 
 #ifdef _MSC_VER
 #pragma comment(lib, "Irrlicht.lib")
+#pragma comment(lib, "BulletCollision_vs2010.lib")
+#pragma comment(lib, "BulletDynamics_vs2010.lib")
+#pragma comment(lib, "BulletSoftBody_vs2010.lib")
+#pragma comment(lib, "LinearMath_vs2010.lib")
 #endif
 
 int main(int argc, char** argv){
